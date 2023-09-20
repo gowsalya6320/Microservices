0eed108ca7506c5e0a5d2681657ea08b49d97b55
gowsalya6320 <lavanyanarayanan11@gmail.com>
2023-09-20 16:02:05 +0530

    cmd

---
+++ Java_Introduction/src/com/aspire/mainmethod/CommandLineArgument.java
@@ -0,0 +1,14 @@
+package com.aspire.mainmethod;
+
+public class CommandLineArgument {
+	
+	public static void main(String[] args) {
+		
+		for(String arg:args) {
+			System.out.println("Arguments:" +args);
+		}
+		
+	}
+	
+
+}
